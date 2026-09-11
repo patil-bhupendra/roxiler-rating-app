0d1eb1a81c8b316bc1573621ac07966b7b3a14fd
Bhupendra Patil <bhupendrarajput1232@gmail.com>
2026-09-11 13:47:46 +0530

    Add user registration API

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,14 @@ require("dotenv").config();
 
 const sequelize = require("./config/database");
 require("./models");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 
