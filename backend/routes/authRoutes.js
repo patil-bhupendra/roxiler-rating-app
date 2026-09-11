89eaed97b4aac44de248e6d1b7fdc055bf0a1f4e
Bhupendra Patil <bhupendrarajput1232@gmail.com>
2026-09-11 15:46:10 +0530

    Remove temporary admin test route

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,10 +1,8 @@
 const express = require("express");
-const {
-  register,
-  login,
-} = require("../controllers/authController");
+const { register, login } = require("../controllers/authController");
 
 const authMiddleware = require("../middleware/authMiddleware");
+const roleMiddleware = require("../middleware/roleMiddleware");
 
 const router = express.Router();
 
